c73ee0e43f9bb6fd7d9b7721230066bc75ddb075
RaMonchi <ramon_sanchez64@hotmail.com>
2024-04-13 21:08:15 +0100

    Versión final

--- C/Hi.c
+++ C/Hi.c
@@ -3,18 +3,20 @@
 /*                                                                          */
 /* DESCRIPCIÓN:                                                             */
 /* Programa de ejemplo para crear un repositirio en GitHub y modificarlo    */
-/* ------------------------------------------------------------------------ */
+/*                                                                          */
 /* HISTORIAL DE DESARROLLO:                                                 */
 /*   Fecha    * Hora * COMENTARIO                                           */
 /* ---------- * -----* ---------------------------------------------------- */
 /* 13/04/2024 * 20:51 * Se escribe una primera versión con el texto "Hola   */
 /*                      mundo" y se crea el repositorio                     */
+/*            *       *                                                     */
+/* 13/04/2024 * 21:04 * Le cambio el texto del saludo                       */
 /* ************************************************************************ */
 
 #include <stdio.h>
 
 int main() {
 
-    printf("Hola mundo!");
+    printf("Hola mundo desde Git y GitHub!");
     return 0;
 }
